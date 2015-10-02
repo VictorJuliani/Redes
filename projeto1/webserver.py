f0744a761ef08e2ea422dd4a2385a1cba0d4049f
Victor Juliani <v_juliani@hotmail.com>
2015-10-02 13:38:39 -0300

    Update index.html and update webserver.py
    Add parameter in request message.

--- projeto1/webserver.py
+++ projeto1/webserver.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# Import modules for CGI handling 
+# Import modules for CGI handling
 import cgi, cgitb
 from socket import *
 
@@ -9,12 +9,14 @@ def clientInstance(address, machine):
 	responses = []
 	serverPort = 12000
 	clientSocket = socket(socket.AF_INET,socket.SOCK_DGRAM)
-	
+
 	# for each marked command, make a request
 	for key in commands:
 		if form.getvalue("m"+str(machine)+"-"+commands[key]):
-			clientSocket.sendto("REQUEST " + str(key) + " ",(address, serverPort)) # TODO add PARAMS
-			
+			# just get parameter if checkbox is checked
+			parameter = form.getvalue("m"+str(machine)+"+"+commands[key])
+			clientSocket.sendto("REQUEST " + str(key) + " "+parameter,(address, serverPort)) # TODO add PARAMS
+
 			# Protocol: RESPONSE X RESULT
 			resMessage = clientSocket.recvfrom(2048)[0]
 			cmds_list.append(key)
@@ -24,8 +26,8 @@ def clientInstance(address, machine):
 
 	return responses, cmds_list
 
-# Create instance of FieldStorage 
-form = cgi.FieldStorage() 
+# Create instance of FieldStorage
+form = cgi.FieldStorage()
 
 commands = {1: "ps", 2: "df", 3: "uptime", 4: "finger"}
 
@@ -34,7 +36,7 @@ html = f.read()
 f.close()
 
 for i in range(3):
-	res, cmds = clientInstance("ADDR" + i, i)	
+	res, cmds = clientInstance("ADDR" + i, i)
 	for j in range(len(cmds)):
 		html.replace("machine"+str(i), "<h4>"+commands[cmds[j]]+"</h4><p>"+res[j]+"</p>")
 
