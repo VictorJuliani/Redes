8e5bd3f682008bb3afa4fa70099304d5d069b8c0
Leonardo Lopes <blopesleo@outlook.com>
2015-10-02 03:36:57 -0300

    Add response.html and update index.html
    Now commands have a parameter field.

--- projeto1/daemon.py
+++ projeto1/daemon.py
@@ -9,14 +9,15 @@ serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
 while 1:
 	message, clientAddress = serverSocket.recvfrom(2048)
-	
+
 	# Protocol: REQUEST X PARAMS
+	#TODO: LELEO (this is wrong?)
 	index = message[8:1] # get X
 	cmd = commands[index] # get commands[X]
 	params = message[10:] # get params
-	
+
 	# TODO: params = re.sub(regex, replace, params)
-	
+
 	res = "RESPONSE " + index " " + subprocess.Popen(cmd, stdout=subprocess.PIPE).stdout.read()
 
 	serverSocket.sendto(res, clientAddress)
