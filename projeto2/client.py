dee3989add072ab88cdd8bf751c2d18922c09595
lbenedette <blopesleo@outlook.com>
2015-11-19 17:46:16 -0200

    Add a simple header(seqnum, acknum, checksum)

--- projeto2/client.py
+++ projeto2/client.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 from socket import socket, AF_INET, SOCK_DGRAM
 import sys
--- projeto2/server.py
+++ projeto2/server.py
@@ -2,13 +2,14 @@
 # -*- coding: utf-8 -*-
 
 from socket import socket, AF_INET, SOCK_DGRAM
-from math import floor
+from math import ceil
 import sys, os
 
 # Add header with a piece of data
-def make_packet(data):
-  packet = 'header '+data
-  return packet
+def make_packet(data, seqnum, acknum, checksum):
+  pkt = ''
+  pkt += str(seqnum)+' '+str(acknum)+' '+str(checksum)+' '+data
+  return pkt
   
 
 def main(argv):
@@ -27,24 +28,30 @@ def main(argv):
     print 'Failed to create socket'
 
   server.bind((host, port))
+  seqnum = 0
+  checksum = 0
+  acknum = 0
 
   while 1:
     filename, addr = server.recvfrom(1024)
     
     fp = open(filename, 'r')
     text = fp.read()
+    # A wild HARD CODE appears
     pkt = []
-    aux = ''
-    for i in range(len(text)):
-      aux += text[i]
-      if (i%10 == 0 and i != 0):
-        pkt.append(aux)
-        aux = ''
+    pktnum = int(ceil(len(text)/10))
+    for i in range(pktnum):
+      start = i * pktnum 
+      end = (i+1) * pktnum
+      print start, end
+      pkt.append(text[start:end])
     
     for i in range(len(pkt)):
-      packet = make_packet(pkt[i])
+      packet= make_packet(pkt[i], seqnum, acknum, checksum)
+      seqnum += 1
       server.sendto(packet, addr)
     server.sendto('end', addr)
+    seqnum = 0
 
   server.close()
 
