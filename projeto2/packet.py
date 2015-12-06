4bdb521a214efae33d685cb254c2b69cd0212acc
Victor Juliani <v_juliani@hotmail.com>
2015-12-05 22:40:38 -0200

    Use PriorityQueues on socket
    Packets are sorted by their segnum. Acks will come first, followed by
    packets that need to be resent and finally by packets unsent.
    Added 'con' header to packets to indicate a socket is connecting.

--- projeto2/packet.py
+++ projeto2/packet.py
@@ -9,20 +9,26 @@ import binascii
 # checksum %s\n
 # end %d\n
 # err %d\n
-HEADER = 'segnum %d\nacknum %d\nchecksum %s\nend %d\nerr %d\n\n'
+HEADER = 'segnum %d\nacknum %d\nchecksum %s\nend %d\nerr %d\ncon %d\n\n'
 
 class Packet:
-	def __init__ (self, data, seg = 0, ack = 0, end = 0, err = 0):
+	def __init__ (self, data, seg = 0, ack = 0, end = 0, err = 0, con = 0):
 		self.data = data
 		self.seg = seg
 		self.ack = ack
 		self.checksum = self.get_CRC32(data)
 		self.end = end
 		self.err = err
+		self.con = con
+
+	def __cmp__(self, other):
+		if other == None:
+			return -1
+    	return cmp(self.seg, other.seg)
 				
 	# build packet string
 	def wrap(self):
-		packet = HEADER % (self.seg, self.ack, self.checksum, self.end, self.err) # build header
+		packet = HEADER % (self.seg, self.ack, self.checksum, self.end, self.err, self.con) # build header
 		packet += self.data # add body
 		return packet
 
@@ -36,6 +42,7 @@ class Packet:
 		self.checksum = header[2].split(' ')[1]
 		self.end = int(header[3].split(' ')[1])
 		self.err = int(header[4].split(' ')[1])
+		self.con = int(header[5].split(' ')[1])
 
 	def validChecksum(self):
 		return self.checksum == self.get_CRC32(self.data)
--- projeto2/reliable_sock.py
+++ projeto2/reliable_sock.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from Queue import Queue
+from Queue import PriorityQueue as PQueue
+from Queue import PriorityQueue
 from packet import Packet
 import binascii, random, threading
 
@@ -12,14 +13,17 @@ class RSock:
 	def __init__ (self, sock, addr):
 		self.init = False
 		self.end = False
+		self.requesting = False
 		self.sock = sock
 		self.addr = addr
-		self.buff = Queue() # packets to send
-		self.fail = Queue() # packets timed out to resend
-		self.waiting = Queue(WINDOW_SIZE) # packets wating for ACK TODO on timeout add to fail queue
-		self.acks = Queue()
 
-		# TODO sort waiting queue by seg! on ack != seg, add packet back to the queue
+		# PQueue will sort packets by segnum, so when inserting in queue, packets will be like:
+		# Acks - segnum = 0
+		# Failed packets (for their segnum is securely < unset packets)
+		# Unsent packets
+		# So ordering is guaranteed and ACKs should be sent immediately
+		self.buff = PQueue() # packets to send
+		self.waiting = PQueue(WINDOW_SIZE) # packets wating for ACK TODO on timeout add to fail queue
 
 		self.seg = 0 # current packet
 		self.ack = 0 # window base point
@@ -29,31 +33,19 @@ class RSock:
 
 	def start(self):
 		while not self.end:
-			src = 0
-			if not self.acks.empty():
-				src = 1
-				packet = self.acks.get(False)
-			elif not self.fail.empty():
-				src = 2
-				packet = self.fail.get(False) # not empty, so won't block...
-			else: # no failed packets or acks to send...
-				self.buff.get(True) # block until another packet is added
-
-			packet = self.nextPacket()
-			if src != 1: # don't block on acks...
-				self.waiting.put(packet) # add packet to waiting queue. it will block if waiting queue is full: all packets are sent & wait for acking
+			packet = self.buff.get(True) # block until another packet is added 
+			# only regular packets should be recent. 
+			# acks require the other side of the socket to be sent again, so we don't need to put on waiting queue
+			if packet.ack == 0 and packet.con == 0:
+				self.waiting.put(packet) # waiting queue will block when full: all packets are sent & wait for acking			
+			
 			wrap = packet.wrap()
 			
 			print "Sending seg " + str(packet.seg) + " to " + str(self.addr)
 			self.sock.sendto(wrap, self.addr)
 
 			# inform Queue we're done with the element we got
-			if src == 0:
-				self.buff.task_done()
-			elif src == 1:
-				self.acks.task_done()
-			else:
-				self.fail.task_done()
+			self.buff.task_done()
 
 			# TODO end packets/end acks CAN FAIL TOO. Use timeout to set end = True when end packet is sent
 			if packet.end:
@@ -66,7 +58,8 @@ class RSock:
 			packet.seg = self.seg
 			self.seg += 1
 		else:
-			packet.ack = self.nextSeg # con packet: seg = 0; ack = nextSeg
+			self.requesting = True
+			packet.con = self.nextSeg # con packet: seg = 0; ack = 0; con = nextSeg
 
 		self.buff.put(packet)
 
@@ -81,7 +74,7 @@ class RSock:
 		self.buff.put(packet)
 		
 	def ackPacket(self, ack, endAck):
-		self.acks.put(Packet('', 0, ack, endAck))
+		self.fail.put(Packet('', 0, ack, endAck)) # add acks on fail queue for this queue is checked before buff queue!
 
 	def wake(self):
 		try:
@@ -97,17 +90,14 @@ class RSock:
 			print "Bad checksum received on seg: " + str(packet.seg) + " ack: " + str(packet.ack)
 			return None
 
-		if not self.init:
+		if packet.con > 0:
+			self.seg = packet.con # set seg with nextSeg received in con field
+			self.ack = packet.con # intitialize ack with nextSeg
 			self.init = True
-			if packet.seg == 0: # con request
+			if not requesting: # this socket received the connection request, then ack it
 				print "Connection request from " + str(self.addr)
-				self.seg = packet.ack # set seg with nextSeg received in ack field
-				self.ack = packet.ack # intitialize ack with nextSeg
-				self.acks.put(Packet('', self.nextSeg, 0, packet.end)) # ack with nextSeg expected on seg field
+				self.fail.put(Packet('', end = packet.end, con = nextSeg)) # ack with nextSeg expected on con field
 				return packet
-			else: # con ack
-				self.seg = packet.seg # set seg with nextSeg of client
-				self.ack = packet.seg # intitialize ack with nextSeg
 		elif (packet.ack > 0 and self.ack == packet.ack): # expected ack!
 			self.ack += 1
 			print "Received expected ack " + str(packet.ack) + " on connection " + str(self.addr)
